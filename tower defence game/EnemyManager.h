84da4a22b0e2bad1a3682886c0c91fee4b5dfa2d
uselesnik <uselesnik.3@gmail.com>
2022-03-16 21:14:50 +0100

    made a class for managing enemies (currently not functionall)

---
+++ tower defence game/EnemyManager.h
@@ -0,0 +1,59 @@
+#pragma once
+#include "Enemy.h"
+
+struct listobject {
+
+};
+
+class EnemyManager
+{
+	/*
+		template <typename T>
+struct Uhu {
+    Uhu* nasl, *prej;
+    T data;
+}; 
+
+template <typename T>
+Uhu<T> *start;
+template <typename T>
+Uhu<T>* zaklj;
+
+template <typename T>
+bool preverjanje() {
+    if (start<T> == nullptr) std::cout << "uhu\n";
+    else std::cout << "spremenjeno\n";
+    return 1;
+}
+
+template <typename T>
+void vnos(T value) {
+    Uhu<T>* novi = new Uhu<T>;
+    novi->data = value;
+    if (start<T> == nullptr && zaklj<T> == nullptr) {
+        start<T> = novi;
+        zaklj<T> = novi;
+        novi->prej = NULL;
+        novi->nasl = NULL;
+        std::cout << "zacetni\n";
+    }
+    else {
+     
+        start<T>->prej = novi;
+        novi->nasl = start<T>;
+        novi->prej = NULL;
+        start<T> = novi;
+        std::cout << "zdej pa mas memory leak ;)\n";
+    }
+}
+template <typename T>
+void loop() {
+    for (Uhu<T>* temp = start<T>; temp != NULL; temp = temp->nasl) {
+        std::cout << temp->data << " ";
+    }
+}
+
+	*/
+
+};
+
